02434c938f0485be752e8f5d98576760971dd7d4
Pedro Vale Ramos <pedro.vale.ramos@gmail.com>
2014-06-26 12:37:06 +0200

    Extracts list template to template script

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    '' : 'showCities',
+    'weather/:country/:city' : 'showWeatherOfCity'
+  },
+
+  showCities: function() {
+    if(!this.cityListView) {
+      this.cityListView = new CityListView({el: $('#content'), collection: new CityCollection()});
+    }
+    this.cityListView.render();
+  },
+
+  showWeatherOfCity: function(country, city) {
+    var model = new City({'country' : country, 'name': city});
+    model.fetch({
+      data: {
+        q : model.get('name') + ',' + model.get('country'),
+        lang: 'sp'
+      },
+      success: function(model) {
+        if(model.get('cod') == 200) {
+          var weatherView = new WeatherView({model: model, el: $('#content')});
+          weatherView.render();
+          console.log('rendered');
+        } else {
+          console.log('error ', model);
+        }
+      },
+      error: function(model){
+        console.log("error with: ", model);
+      }
+    });
+
+  }
+
+});
+
 var CityListView = Backbone.View.extend({
+  template: _.template($('#list_template').html()),
+
   events: {
     'click #add_city' : 'addCity'
   },
 
   render: function() {
-    this.$('#city_list').empty();
+    this.$el.html(this.template());
     var that = this;
     this.collection.each(function(city) {
       var view = that._createItemView(city);
@@ -43,10 +83,6 @@ var CityListItemView = Backbone.View.extend({
 
   template: _.template($('#item_template').html()),
 
-  initialize: function() {
-
-  },
-
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
   },
@@ -60,50 +96,6 @@ var CityListItemView = Backbone.View.extend({
     e.preventDefault();
     var url = 'weather/' + this.model.get('country') + '/' + this.model.get('name');
     router.navigate(url, {trigger: true});
-  }
-});
-
-var City = Backbone.Model.extend({
-  urlRoot: 'http://api.openweathermap.org/data/2.5/weather'
-
-});
-
-var CityCollection = Backbone.Collection.extend({
-
-});
-
-var AppRouter = Backbone.Router.extend({
-  routes: {
-    '' : 'showCities',
-    'weather/:country/:city' : 'showWeatherOfCity'
-  },
-
-  showWeatherOfCity: function(country, city) {
-    var model = new City({'country' : country, 'name': city});
-    model.fetch({
-      data: {
-        q : model.get('name') + ',' + model.get('country'),
-        lang: 'sp'
-      },
-      success: function(model) {
-        console.log("success with: ", model);
-        var weatherView = new WeatherView({model: model, el: $('#content')});
-        weatherView.render();
-        console.log('rendered');
-      },
-      error: function(model){
-        console.log("error with: ", model);
-      }
-    });
-
-  },
-
-  showCities: function() {
-    console.log('show cities');
-    if(!this.cityListView) {
-      this.cityListView = new CityListView({el: $('#content'), collection: new CityCollection()});
-    }
-    this.cityListView.render();
   }
 });
 
@@ -125,9 +117,14 @@ var WeatherView = Backbone.View.extend({
   }
 });
 
+var City = Backbone.Model.extend({
+  urlRoot: 'http://api.openweathermap.org/data/2.5/weather'
+
+});
+
+var CityCollection = Backbone.Collection.extend({
+
+});
+
 var router = new AppRouter();
 Backbone.history.start();
-//var app = new AppView();
-//app.render();
-
-console.log('initialized');
